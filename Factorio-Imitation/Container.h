c125f7a54e2e061d83bb99c47ddd1ec839097d56
JiokKae <ekaktm1@gmail.com>
2021-02-09 16:49:46 +0900

    Add Container InputItem

--- Factorio-Imitation/Container.h
+++ Factorio-Imitation/Container.h
@@ -1,13 +1,21 @@
-#pragma once
+﻿#pragma once
 #include "Structure.h"
+
 class Container : public Structure
 {
+protected:
+	int inventorySize;
+	vector<ItemInfo> vecInventory;
 public:
-	virtual HRESULT Init(int x, int y, DIRECTION direction, bool temp = false) override;
-	virtual void Release();
-	virtual void Update();
-	virtual void FirstRender(Shader* shader) override;
-	virtual void Render(Shader* shader) override;
-	virtual void Render(Shader* shader, float posX, float posY) override;
+	// Need to set inventorySize, ItemId
+	HRESULT Init(int x, int y, DIRECTION direction, bool temp = false) override;
+	virtual void Release() override;
+
+	// 아이템을 컨테이너에 넣는 함수
+	virtual bool InputItem(ItemInfo* item, glm::vec2 pos) override;
+
+	//virtual bool TakeOutItem(ItemInfo* outItem) override { return false; };
+
+	virtual string ToString() override;
 };
 
