d8288a41c6f084c7c3d45ef9eca0fe218e78ba1d
JiokKae <ekaktm1@gmail.com>
2021-09-21 14:55:18 +0900

    c4100 c4458 수정

--- Projects/Factorio-Imitation/Entity.h
+++ Projects/Factorio-Imitation/Entity.h
@@ -11,16 +11,16 @@ public:
 	virtual HRESULT Init();
 	virtual void Release();
 	virtual void Update();
-	virtual void FirstRender(Shader* lpShader) {};
+	virtual void FirstRender(Shader* /*lpShader*/) {};
 	virtual void Render(Shader* lpShader) = 0;
-	virtual void LateRender(Shader* lpShader) {};
+	virtual void LateRender(Shader* /*lpShader*/) {};
 
 	bool IsPassable()			{ return passable; }
-	Vec2 GetPosition()			{ return position; }
+	const Vec2& GetPosition()		{ return position; }
 	virtual FRECT GetCollisionFRect()	{ return FRECT(); }
 	virtual FRECT GetFRect()		{ return FRECT(); }
 
-	virtual void SetPosition(Vec2 position) { this->position = position; }
+	virtual void SetPosition(Vec2 value)	{ position = value; }
 
 	Entity()
 		: passable(false)
