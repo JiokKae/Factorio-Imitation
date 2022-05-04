c179191a1db1d44d0ad861893c9604850205a9d2
JiokKae <ekaktm1@gmail.com>
2022-05-05 01:06:04 +0900

    Factorio-imitation 프로젝트에 의존하던 부분 수정

--- Projects/GLFramework/framework.h
+++ Projects/GLFramework/framework.h
@@ -1,1 +1,9 @@
-#include "../../Factorio-Imitation/framework.h"
+#include "glew/glew.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#pragma warning( push )
+#pragma warning( disable : 4201 )
+#include <glm/glm.hpp>
+#pragma warning( pop )
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
